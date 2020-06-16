796d3fc1adacfdd769f3928cda4a100811a71ebf
gcampbell391 <gcampbell391@gmail.com>
2020-06-15 21:51:56 -0400

    Weather API works

--- app.js
+++ app.js
@@ -1,11 +1,10 @@
-console.log("Starting...")
+require('dotenv').config();
 
-setTimeout(() => {
-    console.log('2 second timer')
-}, 2000)
+const request = require('postman-request');
+const API_KEY = process.env.WEATHER_API_KEY
+const url = `http://api.weatherstack.com/current?access_key=${API_KEY}&query=Woodstock,GA&units=f`
+request({ url: url, json: true }, (error, response) => {
+    console.log(`${response.body.current.weather_descriptions[0]}. It is currently ${response.body.current.temperature} degrees out. It feels like ${response.body.current.feelslike} degrees out.`)
+    // console.log(`It is currently ${response.body.current.temperature} degrees out. There is a ${response.body.current.precip}% chance of rain.`)
 
-setTimeout(() => {
-    console.log('0 second timer')
-})
-
-console.log("Stopping...")
+});
